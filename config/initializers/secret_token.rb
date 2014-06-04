4dee4fef283b43e0fcbd7ef19e0131fd3f8e804d
Alex Zaytsew <ozaits@icloud.com>
2014-06-04 17:08:57 +0400

    initial commit. Create new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarSpinner::Application.config.secret_token = '28f6243fcd670ed44e088b8a5ac428245b95e5076824bff4b9e971943c5988b56315272da736365a82183be05a3a4fe5acc058e95c20e725499ff8e30ac09a44'
